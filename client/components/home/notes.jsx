02d5d8e866f95847e776a83e63762faaccf54293
Joseph Ditton <jditton.atomic@gmail.com>
2022-02-09 12:02:49 -0700

    add bg to note

--- client/components/home/notes.jsx
+++ client/components/home/notes.jsx
@@ -4,7 +4,7 @@ export const Notes = ({ notes, deleteNote }) => {
   return (
     <div className="flex-1">
       {notes.map((note) => (
-        <div key={note.id} className="border-2 rounded p-4">
+        <div key={note.id} className="border-2 rounded p-4 bg-gray-500 text-white">
           {note.contents}
           <div>
             <Button onClick={() => deleteNote(note)}>Delete</Button>
